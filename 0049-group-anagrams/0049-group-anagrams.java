e299f53333d486f85b7c4b5655f3136c5b0d6d51
Jahmil <124407027+Jahswaygo@users.noreply.github.com>
2023-08-14 00:51:16 -0400

    Time: 6 ms (91.01%), Space: 47 MB (65.75%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,48 +1,3 @@
-/*
- * @lc app=leetcode id=49 lang=java
- *
- * [49] Group Anagrams
- *
- * https://leetcode.com/problems/group-anagrams/description/
- *
- * algorithms
- * Medium (66.83%)
- * Likes:    16590
- * Dislikes: 474
- * Total Accepted:    2.1M
- * Total Submissions: 3.2M
- * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
- *
- * Given an array of strings strs, group the anagrams together. You can return
- * the answer in any order.
- * 
- * An Anagram is a word or phrase formed by rearranging the letters of a
- * different word or phrase, typically using all the original letters exactly
- * once.
- * 
- * 
- * Example 1:
- * Input: strs = ["eat","tea","tan","ate","nat","bat"]
- * Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
- * Example 2:
- * Input: strs = [""]
- * Output: [[""]]
- * Example 3:
- * Input: strs = ["a"]
- * Output: [["a"]]
- * 
- * 
- * Constraints:
- * 
- * 
- * 1 <= strs.length <= 10^4
- * 0 <= strs[i].length <= 100
- * strs[i] consists of lowercase English letters.
- * 
- * 
- */
-
-// @lc code=start
 
 import java.util.List;
 import java.util.ArrayList;
@@ -91,6 +46,3 @@ class Solution
         return allGroupedAnagrams;
     }
 }
-
-// @lc code=end
-
