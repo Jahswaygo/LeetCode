141eb15fb0cf7df99b3886de0f610f2e7d8eb3c5
Jahmil <124407027+Jahswaygo@users.noreply.github.com>
2023-08-23 22:55:57 -0400

    Time: 2 ms (83.56%), Space: 43.1 MB (85.13%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=36 lang=java
+ *
+ * [36] Valid Sudoku
+ */
+
+// @lc code=start
+
+
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            HashSet<Character> rowSet = new HashSet<>();
+            HashSet<Character> colSet = new HashSet<>();
+            HashSet<Character> subgridSet = new HashSet<>();
+            
+            for (int j = 0; j < 9; j++) {
+                // Check rows
+                if (board[i][j] != '.' && !rowSet.add(board[i][j])) {
+                    return false;
+                }
+                
+                // Check columns
+                if (board[j][i] != '.' && !colSet.add(board[j][i])) {
+                    return false;
+                }
+                
+                // Check 3x3 subgrids
+                int rowIndex = 3 * (i / 3);
+                int colIndex = 3 * (i % 3);
+                if (board[rowIndex + j / 3][colIndex + j % 3] != '.' &&
+                    !subgridSet.add(board[rowIndex + j / 3][colIndex + j % 3])) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+
+// @lc code=end
+
