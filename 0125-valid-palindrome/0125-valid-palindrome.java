89c7e846e3dd1824688ef3a3d50083b6e6ddc7c7
Jahmil <124407027+Jahswaygo@users.noreply.github.com>
2023-10-11 01:37:14 -0400

    Time: 10 ms (44.92%), Space: 44.7 MB (15.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+
+import java.util.Stack;
+
+class Solution 
+{
+    public boolean isPalindrome(String s) 
+    {
+        boolean isValid = false;
+        StringBuilder forwardString= new StringBuilder();
+        Stack<Character> stackofCharacters=new Stack<>();
+        StringBuilder reversedString = new StringBuilder();
+
+        //Iterating through the given string saving the alphanumeric values.
+        for (char c: s.toCharArray())
+        {
+            if(Character.isLetterOrDigit(c))
+            {
+                forwardString.append(c);
+            }else
+            {
+                continue;
+            }
+        }
+
+        //Pushing string to stack
+        for (char c : forwardString.toString().toCharArray())
+        {
+            stackofCharacters.push(c);
+        }
+        
+        //Popping stack to reverse order
+        while (!stackofCharacters.isEmpty())
+        {
+            reversedString.append(stackofCharacters.pop());   
+        }
+        
+        if (forwardString.toString().equalsIgnoreCase(reversedString.toString()))
+        {
+            isValid = true;
+        }else
+        {
+            isValid = false;
+        }
+        
+        return isValid;
+    }
+}
+// @lc code=end
+
